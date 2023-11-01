499da8be7c46ada07f5cc0df6ed1a384579162a1
Kerry Metayer <KMetayer@appdev.yearup.org>
2023-11-01 13:55:16 -0400

    Dealership

---
+++ src/main/java/Dealership.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Dealership {
+    String name;
+    String address;
+    String phone;
+    ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+    
+}
