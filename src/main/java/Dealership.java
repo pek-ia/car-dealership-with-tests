c944115638fbf2748910cf737888233a0f8147dc
ifarhane <ifarhane@vassar.edu>
2023-11-01 14:52:42 -0400

    finished Phase One

--- src/main/java/Dealership.java
+++ src/main/java/Dealership.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class Dealership {
     String name;
@@ -10,6 +12,36 @@ public class Dealership {
         this.name = name;
         this.address = address;
         this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max){
+        ArrayList<Vehicle> inventoryWithinRange = new ArrayList<>();
+        for(Vehicle vehicle: inventory){
+            if(vehicle.getPrice() >= min && vehicle.getPrice() <= max){
+                inventoryWithinRange.add(vehicle);
+            }
+        }
+        return inventoryWithinRange;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model){
+        ArrayList<Vehicle> inventoryWithinMakeModel = new ArrayList<>();
+        String makeParsed = make.trim();
+        String modelParsed = model.trim();
+        for(Vehicle vehicle: inventory){
+            if(vehicle.getMake().equalsIgnoreCase(makeParsed) && vehicle.getModel().equalsIgnoreCase(modelParsed)){
+                inventoryWithinMakeModel.add(vehicle);
+            }
+        }
+        return inventoryWithinMakeModel;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+        inventory.add(vehicle);
+    }
+
+    public ArrayList<Vehicle> getAllVehicles(){
+        return inventory;
     }
     
 }
